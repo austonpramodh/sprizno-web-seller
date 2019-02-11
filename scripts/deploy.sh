56786bc4c36edccee764ed43354fce3db7082743
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-02-11 08:56:12 +0530

    removed production flag, deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -107,7 +107,7 @@ selectNodeVersion
 echo "installing npm packages"
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
   cd "$DEPLOYMENT_SOURCE"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
