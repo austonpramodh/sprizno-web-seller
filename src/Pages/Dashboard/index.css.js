d61aea7105a97c39ac5f47524a75cc945fbd0e90
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-02-14 09:08:10 +0530

    Navigation Bar added with two components(unfinished)

---
+++ src/Pages/Dashboard/index.css.js
@@ -0,0 +1,25 @@
+const styles = theme => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  brand: {
+    flexGrow: 1,
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+  toolbar: theme.mixins.toolbar,
+});
+
+export default styles;
--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,21 +1,96 @@
-import React, { Fragment } from "react";
-import { withStyles } from "@material-ui/core";
-// import { Redirect } from "react-router-dom";
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import {
+  withStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  List,
+  Drawer,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Button,
+} from "@material-ui/core";
 import styles from "./index.css";
 import Authentication from "../../Utils/Authentication";
+import AddProduct from "../../Components/AddProduct";
+import ListProducts from "../../Components/ListProducts";
 
-const handleLogout = props => {
-  // console.log("props", props);
-  Authentication.logout();
-  props.history.push("/signin");
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedIndex: 0,
+    };
+  }
+
+  render() {
+    const componentsList = [AddProduct, ListProducts];
+    const Components = componentsList.map(Item => <Item />);
+    const { classes } = this.props;
+    const { selectedIndex } = this.state;
+    const handleClick = key => {
+      this.setState({ selectedIndex: key });
+    };
+
+    const handleLogout = () => {
+      const { history } = this.props;
+      Authentication.logout();
+      history.push("/signin");
+    };
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap className={classes.brand}>
+              Sprizno
+            </Typography>
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.toolbar} />
+          <List>
+            {componentsList.map((item, key) => (
+              <ListItem
+                selected={key === selectedIndex}
+                button
+                key={item.meta.name}
+                onClick={() => {
+                  handleClick(key);
+                }}
+              >
+                <ListItemIcon>{<item.meta.icon />}</ListItemIcon>
+                <ListItemText primary={item.meta.name} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <div className={classes.content}>
+          <div className={classes.toolbar} />
+          <Fragment>{Components[selectedIndex]}</Fragment>
+        </div>
+      </div>
+    );
+  }
+}
+Dashboard.propTypes = {
+  classes: PropTypes.object,
+  history: PropTypes.object,
 };
-const Dashboard = props => (
-  <Fragment>
-    Dashboard
-    <button type="button" onClick={() => handleLogout(props)}>
-      logout
-    </button>
-  </Fragment>
-);
+Dashboard.defaultProps = {
+  classes: {},
+  history: {},
+};
 
 export default withStyles(styles)(Dashboard);
