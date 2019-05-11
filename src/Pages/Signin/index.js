0df71a4c9728648b041e574148789dc7ce31307d
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-05-11 17:31:08 +0530

    test commit3 runBuild

--- src/Pages/Signin/index.js
+++ src/Pages/Signin/index.js
@@ -23,7 +23,7 @@ import errMessagesConstants from "../../Utils/Constants/errMessagesConstants";
 import Authentication from "../../Utils/Authentication";
 import Loader from "../../Components/Loader";
 
-// signinRoute test
+// signinRoute
 
 class Signin extends Component {
   state = {
